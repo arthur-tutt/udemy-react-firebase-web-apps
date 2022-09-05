d092479219edd2d26a179bd7b6428c7a4aeb16f9
Arthur Tutt <arthur@arthurtutt.com>
2022-09-05 13:27:32 -0400

    Up to Lesson 161 - but Error: useAuthContext must be used inside an AuthContextProvider

--- demo-project-mgmt-site/the-dojo/src/App.js
+++ demo-project-mgmt-site/the-dojo/src/App.js
@@ -1,9 +1,45 @@
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+
+
+// styles
 import './App.css'
+
+// pages and componeents
+import Dashboard from './pages/dashboard/Dashboard'
+import Create from './pages/create/Create'
+import Login from './pages/login/Login'
+import Project from './pages/project/Project'
+import Signup from './pages/signup/Signup'
+import Navbar from './components/Navbar'
+import Sidebar from './components/Sidebar'
+
 
 function App() {
   return (
     <div className="App">
-
+      <BrowserRouter>
+        <Sidebar />
+        <div className="container"> 
+        <Navbar />
+          <Switch>
+            <Route exact path ="/">
+              <Dashboard />
+            </Route>
+            <Route  path ="/create">
+              <Create />
+            </Route>
+            <Route  path ="/project/:id">
+              <Project />
+            </Route>
+            <Route  path ="/login">
+              <Login />
+            </Route>
+            <Route exact path ="/signup">
+              <Signup />
+            </Route>
+          </Switch>
+          </div>
+      </BrowserRouter>
     </div>
   );
 }
